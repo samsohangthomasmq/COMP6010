85d285a6eb683828b1b23e7723695f2786287b09
samsohangthomasmq <thomas.samsohang@students.mq.edu.au>
2024-07-23 13:26:52 +1000

    Week One 1st Lecture Class - Completed

--- week-one.py
+++ week-one.py
@@ -1,12 +1,44 @@
 # week one - foundation of computer programming
 
-print("\n Welcome to COMP6010")
+# Lecture 01 
+print("\n`````````` Lecture01.py ``````````\n")
+
+print("Welcome to COMP6010")
 print("Hello"+" "+"World") #expression
 
 print(5) # note that numbers don't have the " " around them
 
-
 #expressions with numbers
 print(5+5)
 print(5   +  5)
-print("5     +    5\n")
+print("5     +    5")
+
+print("\n================================ \n")
+
+# Lecture 01b
+print("\n`````````` Lecture01b.py ``````````\n")
+
+# int - a whole number (positive or negative)
+# str - string ... a collection of characters
+# float - 
+# boolean - True or False
+
+# casino door check
+
+is_above_18 = False
+is_dressed_well = True
+
+# type ()
+print("Should we admit this person? " + str(is_above_18 and is_dressed_well))
+is_above_18 = True;
+print("Should we admit this person? " + str(is_above_18 and is_dressed_well))
+
+# type ()
+is_above_18 = True;
+print("Should we admit this person? " + str(is_above_18 and is_dressed_well))
+
+
+print("\n================================ \n")
+
+
+## introduction to number system 
