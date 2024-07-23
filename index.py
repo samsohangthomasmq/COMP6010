07ac0340616af54ecaf693741e1c9b208cf7c5e4
samsohangthomasmq <thomas.samsohang@students.mq.edu.au>
2024-07-23 12:23:39 +1000

    week one init

---
+++ index.py
@@ -0,0 +1,12 @@
+# week one - foundation of computer programming
+
+print("\n Welcome to COMP6010")
+print("Hello"+" "+"World") #expression
+
+print(5) # note that numbers don't have the " " around them
+
+
+#expressions with numbers
+print(5+5)
+print(5   +  5)
+print("5     +    5\n")
